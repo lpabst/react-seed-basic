ae19695bde46a59d2d314fc3677024643dacb97d
loren pabst <lorenpabst@gmail.com>
2018-11-02 17:04:17 -0600

    updated package versions took out unneeded code

--- server/index.js
+++ server/index.js
@@ -1,30 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-var massive = require('massive');
-var session = require('express-session');
-var config = require('./config.js');
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as massive from 'massive';
 
+const config = require('./config.js');
 const app = module.exports = express();
 
 app.use(bodyParser.json());
-app.use(session({
-  secret: config.secret,
-    resave: true,
-    saveUninitialized: false,
-    cookie:{
-      maxAge: (1000*60*60*24*14) //this is 14 days
-    }
-}))
 
-massive(config.connection)
-.then( db => {
-  app.set('db', db);
-})
+// massive(config.connection)
+// .then( db => {
+//   app.set('db', db);
+// })
 
 app.use(express.static(__dirname + './../build'))
 
-var userController = require("./userController.js");
+const userController = require("./userController.js");
 
 //////////Endpoints for the front end
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {unregister} from './registerServiceWorker';
 
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-
-import store from './store';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
